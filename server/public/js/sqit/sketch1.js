5d5d32db788d8308229f05cf99aac65bb8b4a18e
fsImageries <imageries@mail.de>
2021-10-27 11:24:46 +0200

    Added ace editor.

--- server/public/js/sqit/sketch1.js
+++ server/public/js/sqit/sketch1.js
@@ -14,38 +14,46 @@ $("#presser").on("click", () => {
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/javascript");
+editor.setOption({wrap:true});
 
-function setup() {
-  let cnv = createCanvas($(window).width(), 400);
-  cnv.parent("canvasHolder");
-  frameRate(60);
-}
+$("#editor").height("50vh")
 
-function draw() {
-  background($(":root").css("--color-navy-800"));
+// function setup() {
+//   let cnv = createCanvas($(window).width(), 400);
+//   cnv.parent("canvasHolder");
+//   frameRate(60);
+// }
 
-  translate(width / 2, height / 2);
-  noStroke();
+// function draw() {
+//   background($(":root").css("--color-navy-800"));
 
-  // Strobing lights
-  if (on) {
-    fill(255, 0, 0);
-    circle(-(width / 2) / 3, 0, radius * 2);
-  } else {
-    fill(0, 255, 0);
-    circle(width / 2 / 3, 0, radius * 2);
-  }
+//   translate(width / 2, height / 2);
+//   noStroke();
 
-  // Sliding doors
-  stroke(0);
-  strokeWeight(1);
-  fill($(":root").css("--GuardRed"));
-  rect(0 + push, -height / 2, width, height);
+//   // Strobing lights
+//   if (on) {
+//     fill(255, 0, 0);
+//     circle(-(width / 2) / 3, 0, radius * 2);
+//   } else {
+//     fill(0, 255, 0);
+//     circle(width / 2 / 3, 0, radius * 2);
+//   }
 
-  fill($(":root").css("--GuardRed"));
-  rect(0 - push, -height / 2, -width, height);
+//   // Sliding doors
+//   stroke(0);
+//   strokeWeight(1);
+//   fill($(":root").css("--GuardRed"));
+//   rect(0 + push, -height / 2, width, height);
 
-  if (anim) push++;
+//   fill($(":root").css("--GuardRed"));
+//   rect(0 - push, -height / 2, -width, height);
 
-  if (frameCount % interval == 0) on = !on;
-}
+//   if (anim) push++;
+
+//   if (frameCount % interval == 0) on = !on;
+// }
+
+// for (let i = 0; i < radius; i++){
+//   fill(255, 0, 0, i);
+//   circle(-(width / 2) / 2, 0, radius-i);
+// }
