e2a2fd90d0b0b37aaf95f6cb1c748d74666b5062
Wegeleben <marc.wegeleben@softwareone.com>
2021-10-27 16:03:59 +0200

    Added second game.

---
+++ server/public/js/sqit/sketch1.js
@@ -0,0 +1,105 @@
+$("#presser").on("click", () => {
+    console.log("Hi", anim);
+    anim = true;
+    console.log($("#editor").width())
+});
+
+let realWidth = ($(window).width() / 100) * 90
+
+let editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+
+editor.setShowPrintMargin(false);
+editor.setOption({
+    wrap: true,
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    behavioursEnabled: true,
+    hScrollBarAlwaysVisible: false,
+    vScrollBarAlwaysVisible: false
+});
+
+editor.commands.addCommand({
+    name: "...",
+    exec: function () {
+        console.log(editor.getValue());
+    },
+    bindKey: { mac: "cmd-enter", win: "ctrl-enter" },
+});
+
+$("#editor").width(realWidth);
+$("#editor").height("50vh");
+
+let editor2 = ace.edit("editor2");
+editor2.setTheme("ace/theme/monokai");
+editor2.session.setMode("ace/mode/javascript");
+
+editor2.setOption({
+    wrap: true,
+    highlightActiveLine: false,
+    highlightGutterLine: false,
+    hScrollBarAlwaysVisible: false,
+    vScrollBarAlwaysVisible: false
+});
+editor2.setShowPrintMargin(false);
+editor2.renderer.$cursorLayer.element.style.display = "none";
+editor2.setReadOnly(true);
+
+$("#editor2").width(realWidth);
+$("#editor2").height("5vh");
+
+
+const vh_2_px = (vh, height=true) => $(window)[height ? "height": "width"]() * (vh / 100)
+
+
+let on, anim;
+
+let interval = 10;
+let radius = 50;
+let push = 0;
+
+function setup() {
+    let cnv = createCanvas(realWidth, vh_2_px(50));
+    cnv.parent("canvasHolder");
+    frameRate(60);
+}
+
+function draw() {
+    background($(":root").css("--color-navy-800"));
+
+    translate(width / 2, height / 2);
+    noStroke();
+
+    // Strobing lights
+    if (on) {
+        fill(255, 0, 0);
+        circle(-(width / 2) / 3, 0, radius * 2);
+    } else {
+        fill(0, 255, 0);
+        circle(width / 2 / 3, 0, radius * 2);
+    }
+
+    // Sliding doors
+    stroke(0);
+    strokeWeight(1);
+    fill($(":root").css("--GuardRed"));
+    rect(0 + push, -height / 2, width, height);
+
+    fill($(":root").css("--GuardRed"));
+    rect(0 - push, -height / 2, -width, height);
+
+    if (anim) push++;
+
+    if (frameCount % interval == 0) on = !on;
+}
+
+
+function windowResized(){
+    resizeCanvas($(window).width(), 400)
+}
+
+// for (let i = 0; i < radius; i++) {
+//   fill(255, 0, 0, i);
+//   circle(-(width / 2) / 2, 0, radius - i);
+// }
