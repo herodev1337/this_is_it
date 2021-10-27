2650c58584a325aec558d864b7ba04212d39a38d
fsImageries <imageries@mail.de>
2021-10-27 10:45:57 +0200

    Added p5 sketch and ace editor.

--- server/public/js/sqit/sketch1.js
+++ server/public/js/sqit/sketch1.js
@@ -1,19 +1,28 @@
 // import * as $ from "../../../../node_modules/jquery/dist/jquery.slim.js"
 
 let on, anim;
-let interval = 60;
-let radius = 50;
 
+let interval = 10;
+let radius = 50;
 let push = 0;
+
+$("#presser").on("click", () => {
+  console.log("Hi", anim);
+  anim = true;
+});
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
 
 function setup() {
   let cnv = createCanvas($(window).width(), 400);
-  cnv.parent("canvasHolder")
+  cnv.parent("canvasHolder");
   frameRate(60);
 }
 
 function draw() {
-  background(220);
+  background($(":root").css("--color-navy-800"));
 
   translate(width / 2, height / 2);
   noStroke();
@@ -21,19 +30,21 @@ function draw() {
   // Strobing lights
   if (on) {
     fill(255, 0, 0);
-    circle(-(width / 2) / 2, 0, radius * 2);
+    circle(-(width / 2) / 3, 0, radius * 2);
   } else {
     fill(0, 255, 0);
-    circle(width / 2 / 2, 0, radius * 2);
+    circle(width / 2 / 3, 0, radius * 2);
   }
 
   // Sliding doors
-  fill(245);
+  stroke(0);
+  strokeWeight(1);
+  fill($(":root").css("--GuardRed"));
   rect(0 + push, -height / 2, width, height);
 
-  fill(125);
+  fill($(":root").css("--GuardRed"));
   rect(0 - push, -height / 2, -width, height);
-  
+
   if (anim) push++;
 
   if (frameCount % interval == 0) on = !on;
