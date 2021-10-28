599dd8ea8bed4b9903a85ccd95252f761c7bb653
Wegeleben <marc.wegeleben@softwareone.com>
2021-10-28 13:32:36 +0200

    first tictactoe.

--- server/public/js/sqit/sketch2.js
+++ server/public/js/sqit/sketch2.js
@@ -18,6 +18,7 @@ editor.setOption({wrap:true});
 $("#editor").height("50vh")
 
 function setup() {
+    ellipseMode(CENTER)
     let cnv = createCanvas($(window).width(), 400);
     //let cnvInner = createCanvas($(window).width(), 400);
     cnv.parent("canvasHolder");
@@ -27,9 +28,25 @@ function setup() {
 
 function draw() {
     background($(":root").css("--color-navy-800"));
+    fill(255)
 
-    rect(426,0,5,800)
-    rect(852,0,5,800)
-    rect(0,133,1280,5)
-    rect(0,266,1280,5)
+    rect(width/2 -50,75,5,250)
+    rect(width/2 +50,75,5,250)
+    stroke(255)
+    //strokeWeight(5)
+    line(width/2 -25, height/2 -25, width/2 +30, height/2 +30)
+    line(width/2 +30 , height/2 -25, width/2 -25, height/2 +30)
+
+    line(width/2 -70, height/2 -25, width/2 -120, height/2 +30)
+    line(width/2 -120, height/2 -25, width/2 -70, height/2 +30)
+    //line(width/2 +50, height/2 -50, width/2 -100, height/2 +100)
+
+    rect(width/2 -125,height/2 +50,250,5)
+    rect(width/2 -125,height/2 -50,250,5)
+
+
+    noFill()
+    circle(width/2,height/2 + 100,50)
+    circle(width/2,height/2 - 100,50)
+    circle(width/2 -100 ,height/2 + 100,50)
 }
