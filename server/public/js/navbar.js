31ef30d43079e2bdaecd2495762524706919ca31
MarkusRosenberg <markus.rosenberg@softwareone.com>
2021-10-29 13:21:00 +0200

    fade test

--- server/public/js/navbar.js
+++ server/public/js/navbar.js
@@ -4,6 +4,8 @@ var toggleContent = document.getElementById("navbar-toggle-content");
 
 var navDropdownElements = [];
 
+var opacity = 0;
+
 function createDropdown(){
     
     for (var i = 0; i < originalLinks.length; i++){
@@ -16,16 +18,43 @@ function createDropdown(){
 
         navDropdownElements[i].style.display ="block";
         toggleContent.style.display = "none";
+        toggleContent.style.opacity = "0";
     }
 }
 
-function displayDropdown(){
-    if(navDropdownElements[1] === undefined){createDropdown();}
-    if(toggleContent.style.display === "none"){
-        toggleContent.style.display = "flex";
+function displayDropdown(event){
+    if(event.type === "click"){
+        if(navDropdownElements[1] === undefined){createDropdown();}
+        //fadeDropdown();
+        if(toggleContent.style.display === "none"){
+            toggleContent.style.display = "flex";
+        }
+        else {
+            toggleContent.style.display = "none";
+        }
     }
-    else {
+    if(toggleContent.style.display === "flex" && event.type === "mouseleave" && screen.width < screen.height){
         toggleContent.style.display = "none";
     }
 }
 
+/*function fadeDropdown(){
+    for(var i = 0; i < navDropdownElements.length; i++){
+        if(toggleContent.style.display === "none"){
+            if(opacity < 1)setInterval(fadeIn, 100);
+            toggleContent.style.opacity = `${opacity}`;
+        }
+        else{
+            if(opacity > 1)setInterval(fadeOut, 100);
+            toggleContent.style.opacity = `${opacity}`;
+        }
+    }
+}
+
+function fadeIn(){
+    opacity += 0.1;
+}
+
+function fadeOut(){
+    opacity -= 0.1;
+}*/
