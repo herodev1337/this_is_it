9e2bd91e1f6af4f70e97ee4aaa35913b1d19b73e
MarkusRosenberg <markus.rosenberg@softwareone.com>
2021-10-29 10:12:10 +0200

    dropdown menu for resizable purpose

--- server/public/js/navbar.js
+++ server/public/js/navbar.js
@@ -1,4 +1,30 @@
-var originalLinks = getElementsByClassName("navbar-link");
+var originalLinks = document.getElementsByClassName("navbar-link");
 //in navLinks einfügen
 
 var navLinks = [];
+var navDropdownElements = [];
+
+function createDropdown(){
+    for (var i = 0; i < originalLinks.length; i++){
+        navLinks[i] = originalLinks[i].innerHTML;
+    }
+    
+    for (var i = 0; i < navLinks.length; i++){
+        navDropdownElements[i] = document.createElement("div");
+        navDropdownElements[i].innerHTML = navLinks[i];
+        document.getElementById("navbar-toggle-content").appendChild(navDropdownElements[i]);
+        navDropdownElements[i].style.display ="none";
+    }
+}
+
+function displayDropdown(){
+    if(navDropdownElements[1] === undefined){createDropdown();}
+    for (var i = 0; i < navDropdownElements.length; i++){
+        if(navDropdownElements[i].style.display === "none"){
+            navDropdownElements[i].style.display = "block";
+        }
+        else {
+            navDropdownElements[i].style.display = "none";
+        }
+    }
+}
