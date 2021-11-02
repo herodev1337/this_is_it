cd4799231168a523903b4aced864173c55231af2
MarkusRosenberg <markus.rosenberg@softwareone.com>
2021-11-02 09:16:00 +0100

    new design

--- server/public/js/navbar.js
+++ server/public/js/navbar.js
@@ -1,35 +1,64 @@
-var originalLinks = document.getElementsByClassName("navbar-link");
+var originalLinksGames = document.getElementsByClassName("navbar-dropdown-games-elements");
+var originalLinksKnowledge = document.getElementsByClassName("navbar-dropdown-knowledge-elements");
 
 var toggleContent = document.getElementById("navbar-toggle-content");
 
-var navDropdownElements = [];
+var navDropdownElementsGames = [];
+var navDropdownElementsKnowledge = [];
 
 //function for creating the mobile dropdown based on the content of the PC navbar
-function createDropdown(){
+function createDropdownGames(){
     
-    for (var i = 0; i < originalLinks.length; i++){
+    for (var i = 0; i < originalLinksGames.length; i++){
 
         //creating the elements of the dropdown
-        navDropdownElements[i] = document.createElement("a");
+        navDropdownElementsGames[i] = document.createElement("a");
 
         //assigning the original links and HTML to the dropdown elements
-        navDropdownElements[i].href = originalLinks[i].href;
-        navDropdownElements[i].innerHTML = originalLinks[i].innerHTML;
+        navDropdownElementsGames[i].href = originalLinksGames[i].href;
+        navDropdownElementsGames[i].innerHTML = originalLinksGames[i].innerHTML;
 
         //assigning them to the div for the dropdown elements
-        document.getElementById("navbar-toggle-content").appendChild(navDropdownElements[i]);
+        document.getElementById("navbar-toggle-games").appendChild(navDropdownElementsGames[i]);
 
         //setting the default display style of the dropdown elements and the container
-        navDropdownElements[i].style.display ="block";
+        navDropdownElementsGames[i].style.display ="block";
         toggleContent.style.display = "none";
+    }
+}
+
+function createDropdownKnowledge(){
+
+    for (var i = 0; i < originalLinksKnowledge.length; i++){
+    
+        navDropdownElementsKnowledge[i] = document.createElement("a");
+        navDropdownElementsKnowledge[i].href = originalLinksKnowledge[i].href;
+        navDropdownElementsKnowledge[i].innerHTML = originalLinksKnowledge[i].innerHTML;
+        
+        document.getElementById("navbar-toggle-knowledge").appendChild(navDropdownElementsKnowledge[i]);
+
+        navDropdownElementsKnowledge[i].style.display ="block";
+
     }
 }
 
 //jQuery function for showing and hiding the dropdown
 $(document).ready(function() {
 
+    //jquery function for toggleing the navbar content
+    $(document).click((event) => {
+        if(!$(event.target).closest('.navbar-games').length) {$(".navbar-dropdown-games").fadeOut(100);}
+        else {$(".navbar-dropdown-games").fadeToggle(100);}
+
+        if(!$(event.target).closest('.navbar-knowledge').length) {$(".navbar-dropdown-knowledge").fadeOut(100);}
+        else {$(".navbar-dropdown-knowledge").fadeToggle(100);}
+    });
+
     //initializing the creation of the dropdown
-    if(navDropdownElements[1] === undefined){createDropdown();}
+    if(navDropdownElementsGames[1] === undefined){
+        createDropdownGames();
+        createDropdownKnowledge();
+    }
 
     //function for toggleing the display of the dropdown with a fade effect, including hiding the dropdown if the content of it isn't clicked
     $(document).click((event) => {
