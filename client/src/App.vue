96027c49cadb68e58c00fadecc6c755f79a4afc8
herodev <17947793+herodev1337@users.noreply.github.com>
2021-11-05 10:28:28 +0100

    Added vue :D

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar admin=false />
+  <TestComponent show="true" />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import TestComponent from './components/TestComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    TestComponent
+  }
+}
+</script>
---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <p v-show="show">Test</p>
+</template>
+
+<script>
+export default {
+  name: 'TestC',
+}
+</script>
