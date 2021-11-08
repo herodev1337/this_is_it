989ab4f64ed27c9b7221c4b13e71818bebc9da57
MarkusRosenberg <markus.rosenberg@softwareone.com>
2021-11-08 10:56:39 +0100

    added knowledge routes

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './routes/Home';
 import Quiz from './routes/Quiz';
 import Knowledge from './routes/Knowledge';
+import Karriere from './routes/Karriere';
+import Berufsquiz from './routes/Berufsquiz';
+import Berufsswiper from './routes/Berufsswiper';
 import Admin from './routes/Admin';
 
 render(
@@ -13,7 +16,11 @@ render(
       <Route exact path="/" element={<Home />} />
       <Route exact path="/admin" element={<Admin />} />
 
-      <Route exact path="/knowledge" element={<Knowledge />} />
+      <Route exact path="/knowledge" element={<Knowledge />}>
+        <Route exact path="karriere" element={<Karriere />} />
+        <Route exact path="berufsquiz" element={<Berufsquiz />} />
+        <Route exact path="berufsswiper" element={<Berufsswiper />} />
+      </Route>
 
       <Route exact path="/games" element={<Knowledge />}>
         <Route exact path="sqit" element={<Knowledge />}>
---
+++ client/src/routes/Berufsswiper.js
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div>
+      <p>Berufsswiper Seite</p>
+    </div>
+  );
+}
+
