d570fcb5c7740f933a336699aa14eb17da5fe6a0
felikan <felix.herpich@softwareone.com>
2021-11-15 11:41:33 +0100

    adjustable pentagon

--- client/src/components/karriere/contents/Category/Columns/Column1.js
+++ client/src/components/karriere/contents/Category/Columns/Column1.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const Column1 = ({ OnlyCat }) => {
+  const creativity = 0;
+  const technology = 38;
+  const social = 100;
+  const research = 100;
+  const economy = 38;
+
   return (
     <>
       {OnlyCat.map(el => (
         <p id={el.title}>{el.title}</p>
       ))}
-      <div class="penta"></div>
+      <div
+        class="penta"
+        style={{
+          clipPath: `polygon(50% ${creativity}%, 100% ${technology}%, 82% ${social}%, 18% ${research}%, 0% ${economy}%)`,
+        }}
+      ></div>
     </>
   );
 };
