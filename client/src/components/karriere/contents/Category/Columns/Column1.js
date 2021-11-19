d71ea2cb704aca2e444f401ab8b052d464aba7e2
felikan <felix.herpich@softwareone.com>
2021-11-19 09:43:45 +0100

    responsive css

--- client/src/components/karriere/contents/Category/Columns/Column1.js
+++ client/src/components/karriere/contents/Category/Columns/Column1.js
@@ -1,30 +1,47 @@
-import React from 'react';
-import useWindowDimensions from '../../Header, Search, Filter/Window';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Column1 = ({ OnlyCat }) => {
-  // const creativity = 0;
-  // const technology = 100;
-  // const social = 100;
-  // const research = 100;
-  // const economy = 38;
-  const { width } = useWindowDimensions();
-  const rem = Math.floor(width / 75);
+  const ref = useRef(null);
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  const [pentaCenter, setPentaCenter] = useState({left: 0, top: 0})
+  
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: ref.current.clientWidth,
+        height: ref.current.clientHeight,
+      });
+
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <div>
-      <div>
-        
-        {/* //   className="penta"
-        style={{
-           clipPath: `polygon(50% ${creativity}%, ${technology}% 38%, 82% ${social}%, 18% ${research}%, 0% ${economy}%)`,
-         }}
-        */}
-      </div>
-      {OnlyCat.map(el => (
-        <div key={el.title} id={el.title}>
-          {el.title}
+    <div id="svg-container" ref={ref}>
+      <div className="penta">
+        <svg x="0px" y="0px" viewBox="0 0 512.01 512.01">
+          <g>
+            <path
+              d="M507.804,200.28L262.471,12.866c-3.84-2.923-9.131-2.923-12.949,0L4.188,200.28c-3.605,2.773-5.077,7.531-3.648,11.84
+			l93.717,281.92c1.451,4.373,5.525,7.296,10.133,7.296h303.253c4.587,0,8.683-2.944,10.133-7.296l93.717-281.92
+			C512.882,207.789,511.41,203.053,507.804,200.28z"
+            />
+          </g>
+        </svg>
+        <div className="cat-box" style={{left: pentaCenter.left, top: pentaCenter.top}}>
+          <span>.</span>
+        {OnlyCat.map(el => (
+          <div key={el.title} id={el.title}className="cats">
+            {el.title}
+          </div>
+        ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
--- client/src/components/karriere/contents/Header, Search, Filter/Window.js
+++ client/src/components/karriere/contents/Header, Search, Filter/Window.js
@@ -1,24 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
+// Hook
+export default function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
   useEffect(() => {
+    // Handler to call on window resize
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
