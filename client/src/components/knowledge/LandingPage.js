4dd40027762eade6e0b9f9d070b1500cdb86c4b0
felikan <felix.herpich@softwareone.com>
2021-11-25 10:12:55 +0100

    map

--- client/src/components/knowledge/LandingPage.js
+++ client/src/components/knowledge/LandingPage.js
@@ -1,73 +1,35 @@
 import React from 'react';
 import Kategorie from './pictures/Kategorie.png';
-import Carousel from 'react-bootstrap/Carousel';
+import LandingCarousel from './LandingCarousel';
+
+const slide = [
+  {
+    loc: '/karriere',
+    h2: 'Karriereinformationen',
+    p: 'Erfahre mehr über mögliche Karrieren in der IT!',
+    img: Kategorie
+  },
+  {
+    loc: '/knowledge/berufsquiz',
+    h2: 'Berufsquiz',
+    p: 'Überprüfe dein Wissen zu den verschiedenen Berufen',
+    img: '',
+  },
+  {
+    loc: '/knowledge/berufsswiper',
+    h2: 'Berufsswiper',
+    p: 'Welcher Beruf passt zu dir?',
+    img: '',
+  },
+];
 
 const LandingPage = () => {
   return (
     <div id="Landing-container">
       <h1>Knowledge</h1>
-      <Carousel pauseOnHover={true}>
-        <Carousel.Item>
-          <div className="row d-flex justify-content-center">
-            <div
-              className="card"
-              onClick={() => (window.location = '/karriere')}
-            >
-              <h2>Karriereinformationen</h2>
-              <p>Erfahre mehr über mögliche Karrieren in der IT!</p>
-              <img src={Kategorie} />
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="row d-flex justify-content-center">
-            <div
-              className="card"
-              onClick={() => (window.location = '/knowledge/berufsquiz')}
-            >
-              <h2>Berufsquiz</h2>
-              <p>Überprüfe dein Wissen zu den verschiedenen Berufen</p>
-              <img />
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="row d-flex justify-content-center">
-            <div
-              className="card"
-              onClick={() => (window.location = '/knowledge/berufsswiper')}
-            >
-              <h2>Berufsswiper</h2>
-              <p>Welcher Beruf passt zu dir?</p>
-              <img />
-            </div>
-          </div>
-        </Carousel.Item>
-      </Carousel>
+      <LandingCarousel slide={slide} />
     </div>
   );
 };
 
 export default LandingPage;
-
-{
-  /* <div className="card-row">
-        <div className="card" onClick={() => (window.location = '/karriere')}>
-          <h2>Karriereinformationen</h2>
-          <p>Erfahre mehr über mögliche Karrieren in der IT!</p>
-          <img src={Kategorie} />
-        </div>
-      </div>
-      <div className="card-row">
-        <div className="card" onClick={() => (window.location = '/knowledge/berufsquiz')}> 
-          <h2>Berufsquiz</h2>
-          <p>Überprüfe dein Wissen zu den verschiedenen Berufen</p>
-          <img />
-        </div>
-        <div className="card" onClick={() => (window.location = '/knowledge/berufsswiper')}>
-          <h2>Berufsswiper</h2>
-          <p>Welcher Beruf passt zu dir?</p>
-          <img />
-        </div>
-      </div> */
-}
