cb63ed92931184b183caa8fb895348cb3e50a9fe
herodev <17947793+herodev1337@users.noreply.github.com>
2021-12-08 12:43:11 +0100

    Both done

---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import config from 'config';
+import connectDB from './utils/connectDB';
+import log from './utils/logger';
+import routes from './routes';
+import deserializeUser from './middleware/deserializeUser';
+import requestLogger from './middleware/requestLogger';
+
+const port = config.get<number>('port');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(deserializeUser);
+app.use(requestLogger);
+
+app.listen(port, () => {
+  log.info(`Listening on port ${port}`);
+  routes(app);
+});
