481215f9e237f12a95ae5964bf45a39626610f6a
herodev <17947793+herodev1337@users.noreply.github.com>
2021-12-09 11:47:04 +0100

    Old server gone

--- server/utils/networkInterfaces.js
+++
@@ -1,9 +0,0 @@
-/**
- * Returns an Array of local network IPs
- */
-module.exports = () => {
-    return Object.values(require("os").networkInterfaces())
-    .flat()
-    .filter(({ family, internal }) => family === "IPv4" && !internal)
-    .map(({ address }) => address)
-}
